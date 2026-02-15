53d1bedada0a10fbe3670d2e14f091b77b39cbd4
Marin-univ <marin.chesneau31@gmail.com>
2026-02-15 11:43:12 +0000

    start/architecture + devcontainer fonctionel

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="py-8">
+        <v-card class="pa-6" max-width="520">
+          <v-card-title>Boutique OK</v-card-title>
+          <v-card-text>
+            Vue 3 + Vuetify 3 est bien configure.
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
